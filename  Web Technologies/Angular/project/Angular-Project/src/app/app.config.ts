76fe108e15d6970e6af2353905b831be2c476088
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2023-12-29 14:01:03 +0530

    Complete Full stack

---  Web Technologies/Angular/project/Angular-Project/src/app/app.config.ts
+++  Web Technologies/Angular/project/Angular-Project/src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
