d3ee9407ed621a55a6c7a52a54794f1d586a854f
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-02 14:03:18 +0530

    DSA Solid Rectangle patterns

--- DSA JAVA/src/Patterns/SolidRectangle.java
+++ DSA JAVA/src/Patterns/SolidRectangle.java
@@ -9,8 +9,8 @@ package Patterns;
 
 public class SolidRectangle {
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("*"+" ");
             }
             System.out.println();
