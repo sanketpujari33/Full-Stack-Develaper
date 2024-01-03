4112e53b7e8a745a4e23da4253500f7d394734c2
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-03 19:13:54 +0530

    DSA DiamondPattern Advanced Patterns

--- DSA JAVA/src/AdvancedPatterns/DiamondPattern.java
+++ DSA JAVA/src/AdvancedPatterns/DiamondPattern.java
@@ -23,6 +23,7 @@ public class DiamondPattern {
             }
             System.out.println();
         }
+
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <=n-i ; j++) {
                 System.out.print(" ");
