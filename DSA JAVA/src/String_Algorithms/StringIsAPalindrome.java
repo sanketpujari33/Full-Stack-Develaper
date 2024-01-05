ca8f995e3ec33144726cc7368989287ebafe9fb4
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-05 18:10:00 +0530

    DSA String Is A Palindrome  String_Algorithms

---
+++ DSA JAVA/src/String_Algorithms/StringIsAPalindrome.java
@@ -0,0 +1,22 @@
+package String_Algorithms;
+
+public class StringIsAPalindrome {
+    public static boolean isPalindrome(String word){
+        char [] charArray= word.toCharArray();
+        int start=0;
+        int end=word.length()-1;
+        while (start<end){
+            if(charArray[start]!=charArray[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str="madam";
+        System.out.println(isPalindrome(str));
+    }
+}
