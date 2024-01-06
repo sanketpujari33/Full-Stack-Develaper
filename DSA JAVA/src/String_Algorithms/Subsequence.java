7c0bcb92a42c53744b7793e812cf8974c0feb6fd
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-07 00:45:05 +0530

    DSA Subsequence String_Algorithms

---
+++ DSA JAVA/src/String_Algorithms/Subsequence.java
@@ -0,0 +1,19 @@
+package String_Algorithms;
+
+public class Subsequence {
+    public static boolean isSubseq(String str, String seq){
+        int i=0;
+        int j=0;
+        while (i<str.length() && j< seq.length()){
+            if(str.charAt(i)==seq.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        return j==seq.length();
+    }
+    public static void main(String[] args) {
+        String str="abcde", seq="ace";
+        System.out.println(isSubseq(str,seq));
+    }
+}
