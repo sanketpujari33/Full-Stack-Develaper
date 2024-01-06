6a2e34ac89f99e9b6cc2fa2191de4b56e7c4dd41
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-06 07:30:40 +0530

    DSA Binary Searching

---
+++ DSA JAVA/src/Searching/BinarySearch.java
@@ -0,0 +1,32 @@
+package Searching;
+
+public class BinarySearch {
+
+    public static int search(int[] arr, int target) {
+        int low=0;
+        int high=arr.length;
+        while (low<=high) {
+            int mid=(high+low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(target<arr[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = 7;
+        int result = search(arr, target);
+        if (result == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found index: " + result);
+        }
+    }
+}
