e0722d8467cc65b858d0e4f87d87eec1c6bfd11c
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-06 09:15:33 +0530

    DSA Merge Sorted Arrays Sorting

--- DSA JAVA/src/Sorting/MergeSort.java
+++ DSA JAVA/src/Sorting/MergeSort.java
@@ -32,14 +32,17 @@ public class MergeSort {
                 arr[j]=merge[i];
         }
     }
+    public static void printArray(int[] arr) {
+        for (int i=0; i< arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[]={ 6, 3, 9, 5, 2, 8};
         int size= arr.length;
 
         divide(arr,0,size-1);
-
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i]+" ");
-        }
+        printArray(arr);
     }
 }
