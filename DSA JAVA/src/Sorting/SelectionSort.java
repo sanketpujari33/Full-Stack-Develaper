28dd8e6bd740431531a5e036b32b3f8da30d8dc3
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-06 08:21:11 +0530

    DSA SelectionSort Sorting

--- DSA JAVA/src/Sorting/SelectionSort.java
+++ DSA JAVA/src/Sorting/SelectionSort.java
@@ -1,10 +1,9 @@
 package Sorting;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int arr[]={5, 6, 8, 4, 1, 2, 7, 3, 9, 10};
+    public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
-           int smallest=i;
+            int smallest=i;
             for (int j = i+1; j <arr.length; j++) {
                 if (arr[smallest]>arr[j]){
                     smallest=j;
@@ -14,8 +13,16 @@ public class SelectionSort {
             arr[smallest]=arr[i];
             arr[i]=temp;
         }
+    }
+    public static void printArray(int[] arr) {
         for (int i=0; i< arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={5, 6, 8, 4, 1, 2, 7, 3, 9, 10};
+        selectionSort(arr);
+        printArray(arr);
     }
 }
