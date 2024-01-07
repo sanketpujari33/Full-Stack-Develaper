e10fafe9d43f4e8841219f4e3d84992cad7fd3d1
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-07 17:59:29 +0530

    DSA Remove Element Array

---
+++ DSA JAVA/src/Array/RemoveElement.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class RemoveElement {
+    public static int removeElement(int arr[],int val) {
+        int newArr[] = new int[arr.length];
+        int i = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != val) {
+                arr[i] = arr[j];
+                newArr[i] = arr[j];
+                i++;
+            }
+        }
+        for(int k=0; k< newArr.length; k++){
+            System.out.print(newArr[k]+" ");
+        }
+        System.out.println();
+        return i;
+    }
+    public static void main(String[] args) {
+     int arr[]={0,1,2,2,3,0,4,2};
+     int val=2;
+        System.out.println(removeElement(arr, val));
+    }
+
+}
