fcd71674bb1eab8fcec0d57f43d49d27920d74cc
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-07 16:22:53 +0530

    DSA Reverse Integer Basic Programing

---
+++ DSA JAVA/src/BasicPrograming/ReverseInteger.java
@@ -0,0 +1,22 @@
+package BasicPrograming;
+
+public class ReverseInteger {
+    public static long reverseInteger(int number){
+        boolean isNagative=number<0;
+        if (isNagative){
+            number=number*-1;
+        }
+        long reverse=0;
+        int lastDigit;
+        while (number > 0){
+            lastDigit=number%10;
+            reverse=reverse*10+lastDigit;
+            number=number/10;
+        }
+        return isNagative ? reverse * -1 :reverse;
+    }
+    public static void main(String[] args) {
+        int number=-1234;
+        System.out.println(reverseInteger(number));
+    }
+}
