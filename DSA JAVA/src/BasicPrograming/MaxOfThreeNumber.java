1153450f42567ca689d8a33b6b7a2be634b17978
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-09 13:31:34 +0530

    DSA Find Max Of Three Number BasicPrograming

---
+++ DSA JAVA/src/BasicPrograming/MaxOfThreeNumber.java
@@ -0,0 +1,24 @@
+package BasicPrograming;
+
+public class MaxOfThreeNumber {
+    public static void maxOfThree(int a, int b, int c){
+        if(a>b){
+            if(a>c){
+                System.out.println(a);
+            }
+            else {
+                System.out.println(c);
+            }
+        }
+        else if(b>c) {
+            System.out.println(b);
+        }else {
+            System.out.println(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        int a=100, b=150, c=40;
+        maxOfThree(a,b,c);
+    }
+}
