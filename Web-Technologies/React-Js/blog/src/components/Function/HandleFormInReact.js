101ade3f28f13f1d0241996c241230555c84c687
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-16 17:52:48 +0530

    Handle Form In React

--- Web-Technologies/React-Js/blog/src/components/Function/HandleFormInReact.js
+++ Web-Technologies/React-Js/blog/src/components/Function/HandleFormInReact.js
@@ -3,8 +3,8 @@ function HandleFormInReact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-   const [term, setTerm]=useState(false);
-   const [interest, setInterest]=useState("");
+    const [term, setTerm] = useState(false);
+    const [interest, setInterest] = useState("");
     function getFormData(e) {
         console.warn(name, email, password, term, interest);
         e.preventDefault();
@@ -13,16 +13,16 @@ function HandleFormInReact() {
         <>
             <h1>Handle Form In React</h1>
             <form onSubmit={getFormData}>
-                <input type="text" placeholder="Enter Name" onChange={(e)=>setName(e.target.value)}/><br/><br/>
-                <input type="email" placeholder="Enter Email" onChange={(e)=>setEmail(e.target.value)}/><br/><br/>
-                <input type="password" placeholder="Enter Password" onChange={(e)=>setPassword(e.target.value)}/><br/><br/>
-                <select onChange={(e)=>setInterest(e.target.value)}>
+                <input type="text" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} /><br /><br />
+                <input type="email" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} /><br /><br />
+                <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} /><br /><br />
+                <select onChange={(e) => setInterest(e.target.value)}>
                     <option >select Option</option>
                     <option >Marvel</option>
                     <option >Dc</option>
-                </select><br/><br/>
-                <input type="checkbox" onChange={(e)=>setTerm(e.target.checked)}/><span>Accept Term and Conditions</span><br/><br/>
-                <button type="submit">Submit</button><br/><br/>
+                </select><br /><br />
+                <input type="checkbox" onChange={(e) => setTerm(e.target.checked)} /><span>Accept Term and Conditions</span><br /><br />
+                <button type="submit">Submit</button><br /><br />
             </form>
         </>
     );
