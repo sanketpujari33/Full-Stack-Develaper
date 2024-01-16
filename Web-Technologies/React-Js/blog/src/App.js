9574d5600ee7955b655b34aa25de2c82074f0ddc
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-16 12:38:36 +0530

    FunctionComponent

--- Web-Technologies/React-Js/blog/src/App.js
+++ Web-Technologies/React-Js/blog/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import FunctionComponent from './components/FunctionComponent';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FunctionComponent/>
     </div>
   );
 }
---
+++ Web-Technologies/React-Js/blog/src/components/FunctionComponent.js
@@ -0,0 +1,16 @@
+function FunctionComponent() {
+    return (
+        <>
+            <h1>Function Component</h1>
+            <User />
+        </>
+    );
+}
+function User() {
+    return (
+        <>
+            <h1>User Component</h1>
+        </>
+    );
+}
+export default FunctionComponent;
