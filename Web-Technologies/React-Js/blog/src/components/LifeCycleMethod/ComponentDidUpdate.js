f16b88f3990a41bac81a83e538d6959fb57b5376
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-17 18:10:11 +0530

    ComponentDidUpdate

---
+++ Web-Technologies/React-Js/blog/src/components/LifeCycleMethod/ComponentDidUpdate.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export class ComponentDidUpdate extends React.Component {
+    constructor() {
+        super();
+        // console.warn("constructor");
+        this.state={
+            count:0
+        }
+    }
+    componentDidMount() {
+        // console.warn("ComponentDidMount");
+    }
+    componentDidUpdate(preProps, preState, snapShot) {
+        // console.warn("ComponentDidUpdate",snapShot);
+        // console.warn("ComponentDidUpdate",preState);
+        // if(preState.count===this.state.count){
+        //     alert("data is already same");
+        // }
+        // else{
+        //     alert("data is changed");
+        // }
+        if(this.state.count < 10)
+        this.setState({count:this.state.count+1})
+    }
+    render() {
+        // console.warn("Render")
+        return (
+            <div>
+                <h1>ComponentDidUpdate</h1>
+                <h2>{this.state.count}</h2>
+                    <button onClick={()=>this.setState({count:1})}>Update</button>
+            </div>
+        );
+    };
+};
--- Web-Technologies/React-Js/blog/src/components/LifeCycleMethod/LifeCycleMethod.js
+++ Web-Technologies/React-Js/blog/src/components/LifeCycleMethod/LifeCycleMethod.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
-import { ComponentDidMount } from "./ComponentDidMount";
-import { CunstructorLifeCycle } from './CunstructorLifeCycle';
-import { RenderLifeCyle } from "./RenderLifeCyle";
+import React from "react";
+import { ComponentDidUpdate } from "./ComponentDidUpdate";
+// import { ComponentDidMount } from "./ComponentDidMount";
+// import { CunstructorLifeCycle } from './CunstructorLifeCycle';
+// import { RenderLifeCyle } from "./RenderLifeCyle";
 
 function LifeCycleMethod() {
-  const [name, setName] = useState("Sanket");
-  return (
-    <div style={{ margin: 10, padding: 10, backgroundColor: "darksalmon" }}>
-      <h1>LifeCycle Method</h1><CunstructorLifeCycle/>
-      <RenderLifeCyle name={name} />
-      <button onClick={() => setName("SanketPujari")}>Update Name</button>
-      <ComponentDidMount/>
-    </div>
-  );
+    //   const [name, setName] = useState("Sanket");
+    return (
+        <div style={{ margin: 10, padding: 10, backgroundColor: "darksalmon" }}>
+            <h1>LifeCycle Method</h1>
+        {/*<CunstructorLifeCycle/>
+        <RenderLifeCyle name={name} />
+        <button onClick={() => setName("SanketPujari")}>Update Name</button>
+        <ComponentDidMount/>*/}
+        <ComponentDidUpdate/>
+
+        </div>
+    );
 }
 export default LifeCycleMethod;
