75041cd13cd76269e65d87a86e0646e3936f8227
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-17 16:59:47 +0530

    Render Life Cycle

--- Web-Technologies/React-Js/blog/src/components/LifeCycleMethod/LifeCycleMethod.js
+++ Web-Technologies/React-Js/blog/src/components/LifeCycleMethod/LifeCycleMethod.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-// import { CunstructorLifeCycle } from './CunstructorLifeCycle';
+import { CunstructorLifeCycle } from './CunstructorLifeCycle';
 import { RenderLifeCyle } from "./RenderLifeCyle";
 
 function LifeCycleMethod() {
-    const [name, setName] = useState("Sanket");
+  const [name, setName] = useState("Sanket");
   return (
     <div style={{ margin: 10, padding: 10, backgroundColor: "darksalmon" }}>
-      <h1>LifeCycle Method</h1>
-      {/* <CunstructorLifeCycle/>*/}
+      <h1>LifeCycle Method</h1><CunstructorLifeCycle/>
       <RenderLifeCyle name={name} />
-      <button onClick={() => setName("SanketPujari")}>
-        Update Name
-      </button>
+      <button onClick={() => setName("SanketPujari")}>Update Name</button>
     </div>
   );
 }
