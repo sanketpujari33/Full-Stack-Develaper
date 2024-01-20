7bb652af8fb80205dfb4f6289bc2763d8415094d
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-20 13:02:51 +0530

    UseRef Function Component

--- Web-Technologies/React-Js/blog/src/App.js
+++ Web-Technologies/React-Js/blog/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 // import LifeCycleMethod from "./components/lifeCycleMethod/LifeCycleMethod.js";
-import ClassComponent from "./components/class/ClassComponent";
+// import ClassComponent from "./components/class/ClassComponent";
 // import FunctionComponent from './components/function/FunctionComponent';
-// import Hook from './components/hook/Hook';
+import Hook from './components/hook/Hook';
 
 function App() {
   return (
     <div className="App">
        {/* <FunctionComponent/> */}
-      <ClassComponent /> 
+      {/* <ClassComponent />  */}
       {/* <LifeCycleMethod/> */}
-       {/* <Hook/> */}
+       <Hook/>
     </div>
   );
 }
--- Web-Technologies/React-Js/blog/src/components/hook/UseRefFunctionComponent.js
+++ Web-Technologies/React-Js/blog/src/components/hook/UseRefFunctionComponent.js
@@ -1,12 +1,20 @@
 import React ,{ useRef } from "react";
 
 function UseRefFunctionComponent() {
-    useRef(()=>{
-        console.log("Use Ref");
-    })
+    let inputRef=useRef(null);
+    function handleInput(){
+        // inputRef.current.value="100";
+        // inputRef.current.style.color="red";
+        // inputRef.current.style.backgroundColor="yellow";
+        // inputRef.current.focus();
+        inputRef.current.style.display="none";
+        
+    }
     return ( 
         <>
         <h1>UseRef Function Component</h1>
+        <input type="text" ref={inputRef}/>
+        <button onClick={handleInput}>Check Ref</button>
         </>
      );
 }
