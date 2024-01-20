33316ce8110927d301f9f7d5055055386212a109
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-20 17:02:34 +0530

    Router

--- Web-Technologies/React-Js/blog/src/App.js
+++ Web-Technologies/React-Js/blog/src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 // import LifeCycleMethod from "./components/lifeCycleMethod/LifeCycleMethod.js";
 // import ClassComponent from "./components/class/ClassComponent";
 // import FunctionComponent from './components/function/FunctionComponent';
-import Hook from './components/hook/Hook';
-
+// import Hook from './components/hook/Hook';
+import Routess from "./components/router/Routess";
 function App() {
   return (
     <div className="App">
        {/* <FunctionComponent/> */}
       {/* <ClassComponent />  */}
       {/* <LifeCycleMethod/> */}
-       <Hook/>
+       {/* <Hook/> */}
+       <Routess/>
     </div>
   );
 }
