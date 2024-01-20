d504a037c688b5e58865dc3d4f3ac7aa7043e02f
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-20 17:13:08 +0530

    Page404

---
+++ Web-Technologies/React-Js/blog/src/components/pages/Page404.js
@@ -0,0 +1,9 @@
+function Page404() {
+    return ( 
+        <>
+        <h1>404</h1>
+        </>
+     );
+}
+
+export default Page404;
--- Web-Technologies/React-Js/blog/src/components/router/Routess.js
+++ Web-Technologies/React-Js/blog/src/components/router/Routess.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Navbar from './Navbars';
+import Page404 from '../pages/Page404';
 function Routess() {
     return (
         <>
@@ -13,6 +14,8 @@ function Routess() {
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About/>} />
                     <Route path="/contact" element={<Contact/>} />
+                    {/* <Route path="/*" element={<Page404/>} /> */}
+                    <Route path="/*" element={<Navigate to="/"/>} />
                 </Routes>
             </BrowserRouter>
         </>
