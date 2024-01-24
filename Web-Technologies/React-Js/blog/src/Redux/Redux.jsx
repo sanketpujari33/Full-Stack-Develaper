af7eca4d9ed04b1dc4911d244ba3cf599e30e1d0
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-01-24 12:35:34 +0530

    Start Redux

---
+++ Web-Technologies/React-Js/blog/src/Redux/Redux.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../App.css'
+import {User} from './User'
+function Redux(){
+    return (
+        <div className='App'>
+            <h1>Redux</h1>
+            <User/>
+        </div>
+    )
+}
+export default Redux;
--- Web-Technologies/React-Js/blog/src/index.js
+++ Web-Technologies/React-Js/blog/src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import  Redux  from './components/Redux/Redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Redux/>
   </React.StrictMode>
 );
 
