c8afce4e10fe36e66bac9cc1974b1eefee273498
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-02-16 20:06:54 +0530

    gitignore file update

---
+++ Web-Technologies/Backend/Tutorial/index.js
@@ -0,0 +1,62 @@
+// var a=20;
+// var b=30;
+// var c=40;
+
+// console.log(a+b+c);
+// const fs =require ('fs');
+// fs.writeFileSync("./hello.txt","console.log('Hello, sanket')");
+
+// const read=fs.readFileSync("./hello.js")
+
+// console.log(read.toString())
+
+// fs.readFile('./hello.js','utf8', (err,data)=>{
+//         console.log(data);
+        
+//     } )
+
+// const a=fs.readFileSync('hello.txt');
+// console.log(a.toString());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// const os =require('os');
+
+
+// console.log(os.freemem());//Free Momaray
+
+// console.log(os.homedir());//Home Directory
+
+// console.log(os.hostname());//Host name;
+
+// console.log(os.platform());//Plat Form
+
+// console.log(os.release());//Release Virsion For Div
+
+// console.log(os.type());// retuen Oprating System Name
+
+
+
+
+// const http= require('http')
+
+// http.createServer((req, resp)=>{
+    
+//     resp.write("<h1 style={colour:'red'}>This is sanket pujari</h1>");
+//     resp.end;
+// }).listen(4500)
+
+
+// const colour = require('colors');
+
+// console.log('server Start'.red)
+
+// Importing module 
+const chalks = require("halk"); 
+
+// Printing the text 
+console.log(chalks.red("aayush")) 
+console.log(chalks.red.underline("aayush")) 
+console.log(chalks.red.underline.bold("GFG"))
