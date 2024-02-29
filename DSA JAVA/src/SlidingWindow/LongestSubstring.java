bf5499403bc4ffc9bca984117d54d7206f084a22
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-02-29 10:36:48 +0530

    java

--- DSA JAVA/src/SlidingWindow/LongestSubstring.java
+++ DSA JAVA/src/SlidingWindow/LongestSubstring.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestSubstring {
-    public static  void LongestSubstring(String str){
-        Map<Character,Integer> map=new HashMap<>();
-        int maxLength=0;
-        int start=0;
-        for (int end = 0; end < str.length(); end++) {
-            char rightChar=str.charAt(end);
-            if(map.containsKey(r))
-
-        }
-    }
+//    public static  void LongestSubstring(String str){
+//        Map<Character,Integer> map=new HashMap<>();
+//        int maxLength=0;
+//        int start=0;
+//        for (int end = 0; end < str.length(); end++) {
+//            char rightChar=str.charAt(end);
+//            if(map.containsKey(r))
+//
+//        }
+//    }
     public static void main(String[] args) {
 
     }
