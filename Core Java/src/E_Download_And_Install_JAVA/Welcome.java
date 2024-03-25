e1ae547738b395db6ad84c521a42a60b08c61d09
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-03-25 23:59:59 +0530

    add notes

---
+++ Core Java/src/E_Download_And_Install_JAVA/Welcome.java
@@ -0,0 +1,8 @@
+package E_Download_And_Install_JAVA;// TO display Welcome message onto the screen
+
+class Welcome{
+	public static void main(String[] args)
+	{
+	System.out.println("Welcome");	
+	}
+}
---
+++ Project/MyProj/src/Welcome.java
@@ -0,0 +1,6 @@
+// To display Welcome message
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+}
